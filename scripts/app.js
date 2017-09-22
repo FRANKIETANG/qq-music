48ca4a3896c250c041278b76401eace929edce29
FRANKIETANG <350558468@qq.com>
2017-09-22 17:42:23 +0800

    电台模块

--- scripts/app.js
+++ scripts/app.js
@@ -5,13 +5,30 @@
         .then(render)
 
     function render(json){
-        let slides = json.data.slider.map(slide => {
+        renderSlider(json.data.slider)
+        renderRadios(json.data.radioList)
+    }
+
+    function renderSlider(slides) {
+        slides = slides.map(slide => {
             return { link: slide.linkUrl, image: slide.picUrl }
         })
         new Slider({
             el: document.querySelector('#slider'),
             slides
-        })
+        })        
+    }
+
+    function renderRadios(radios) {
+        document.querySelector('.radios .list').innerHTML = radios.map(radio => `
+            <div class="list-item">
+                <div class="list-madia">
+                    <img src="${radio.picUrl}">
+                    <span class="icon icon_play"></span>
+                </div>
+                <div class="list-title">${radio.Ftitle}</div>
+            </div>        
+        `).join('')
     }
 
 })()
